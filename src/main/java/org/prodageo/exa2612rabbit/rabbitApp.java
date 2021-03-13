b9e6d7c1aa59448ef713e41f349661a2321e38b9
BAUCHER Frederic <frederic.baucher@cegedim.com>
2021-03-13 16:48:03 +0100

    enfin ca ecrit dans le buffer

--- src/main/java/org/prodageo/exa2612rabbit/rabbitApp.java
+++ src/main/java/org/prodageo/exa2612rabbit/rabbitApp.java
@@ -1,5 +1,6 @@
 package org.prodageo.exa2612rabbit;
-import org.prodageo.exa2612lib.sharedCounter ;
+
+// FBABLIB import org.prodageo.exa2612lib.sharedCounter ;
 
 /*
 import java.util.concurrent.Executors;
@@ -19,34 +20,72 @@ public class rabbitApp
         int sc = sharedCounter.theSharedCounter ;
         int i = 0 ;
         int secondsToSleep = 1 ; // 1 for rabbit
+		char start = '0' ;
 
-        try
-        {
-            sharedCounter smc = new sharedCounter ( ) ;
+		if ( args.length > 0 )
+		{
+			String arg0  = args[0] ;
+			start = arg0.charAt(0) ;
+		}
+		
+		if ( start == '-' )
+		{
+
+			try
+			{
+				sharedCounter smc = new sharedCounter ( '!' ) ;
+				
+				int j = 0 ;
+				while ( j < 15 ) {
+					char c = smc.getStoredCharacterAt ( j ) ;
+					System.out.println( j + " : " + c );
+					j = j + 1 ;
+				}
+			}        
+			catch ( Throwable e)
+			{
+				e.printStackTrace();
+			}
+		}
+		else
+		{
+			try
+			{
+				sharedCounter smc = new sharedCounter ( start ) ;
 
 
 
-            while ( i < 7 ) {
+				while ( i < 4 ) {
 
-                // sc = sc + 1 ;
-                sc = smc.incrementSharedMemoryCounter() ;
+					// sc = sc + 1 ;
+					sc = smc.incrementSharedMemoryCounter( ) ;
 
-                System.out.println( currentTime () + " : " + sc );
+					System.out.println( currentTime () + " : " + sc );
 
-                // wait a little bit
-                i = i + 1 ;
-                try {
-                    Thread.sleep(secondsToSleep * 1000);
-                } catch (InterruptedException ie) {
-                    Thread.currentThread().interrupt();
-                }
-            }
+					// wait a little bit
+					i = i + 1 ;
+					try {
+						Thread.sleep(secondsToSleep * 10000);
+					} catch (InterruptedException ie) {
+						Thread.currentThread().interrupt();
+					}
+				}
+				
+				
+				int j = 0 ;
+				while ( j < 15 ) {
+					char c = smc.getStoredCharacterAt ( j ) ;
+					System.out.println( j + " : " + c );
+					j = j + 1 ;
+				}
+				
 
-        }        
-        catch ( Throwable e)
-        {
-            e.printStackTrace();
-        }
+			}        
+			catch ( Throwable e)
+			{
+				e.printStackTrace();
+			}
+		}
     }
 
     private static String currentTime ()
